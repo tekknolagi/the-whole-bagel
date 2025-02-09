62250b13cda3b9b883f5a6d70b51cc5a3b9faeb6
Max Bernstein <max@bernsteinbear.com>
2025-02-09 12:38:17 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,155 @@
+struct Lexer<'a> {
+    chars: std::iter::Peekable<std::str::Chars<'a>>,
+}
+
+#[derive(Debug, PartialEq)]
+enum Token {
+    Ident(String),
+    Print,
+    Str(String),
+    Int(i64),
+    Bool(bool),
+    Float(f64),
+    Eof,
+    Semicolon,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Equal,
+    EqualEqual,
+    Plus,
+    Minus,
+    Star,
+    ForwardSlash,
+    BackSlash,
+    Bang,
+    Or,
+    And,
+    Var,
+    If,
+    While,
+    For,
+    Fun,
+    Return,
+    Class,
+    LParen,
+    RParen,
+    LCurly,
+    RCurly,
+}
+
+#[derive(Debug, PartialEq)]
+enum LexError {
+    UnexpectedChar(char),
+    UnterminatedStringLiteral,
+}
+
+const NUMBER_BASE: u32 = 10;
+impl<'a> Lexer<'a> {
+    fn from_str(source: &'a str) -> Lexer<'a> {
+        Lexer { chars: source.chars().peekable() }
+    }
+
+    fn next(&mut self) -> Result<Token, LexError> {
+        loop {
+            match self.chars.next() {
+                None => { return Ok(Token::Eof); }
+                Some(c) if c.is_whitespace() => { continue; }
+                Some(c) if c.is_alphabetic() => { return self.read_ident(c); }
+                Some(c) if c.is_digit(NUMBER_BASE) => { return self.read_int(c.to_digit(NUMBER_BASE).unwrap() as i64); }
+                Some('"') => { return self.read_string(); }
+                Some(';') => { return Ok(Token::Semicolon); }
+                Some('+') => { return Ok(Token::Plus); }
+                Some('/') => { return Ok(Token::ForwardSlash); }
+                Some('!') => { return Ok(Token::Bang); }
+                Some('(') => { return Ok(Token::LParen); }
+                Some(')') => { return Ok(Token::RParen); }
+                Some('{') => { return Ok(Token::LCurly); }
+                Some('}') => { return Ok(Token::RCurly); }
+                Some('<') => {
+                    return Ok(match self.chars.peek() {
+                        Some('=') => { self.chars.next(); Token::LessEqual }
+                        _ => Token::Less,
+                    });
+                }
+                Some('>') => {
+                    return Ok(match self.chars.peek() {
+                        Some('=') => { self.chars.next(); Token::GreaterEqual }
+                        _ => Token::Greater,
+                    });
+                }
+                Some('=') => {
+                    return Ok(match self.chars.peek() {
+                        Some('=') => { self.chars.next(); Token::EqualEqual }
+                        _ => Token::Equal,
+                    });
+                }
+                Some(c) => { return Err(LexError::UnexpectedChar(c)); }
+            }
+        }
+    }
+
+    fn read_ident(&mut self, c: char) -> Result<Token, LexError> {
+        let mut result: String = "".into();
+        result.push(c);
+        loop {
+            match self.chars.peek() {
+                Some(c) if c.is_alphabetic() => result.push(*c),
+                _ => break,
+            }
+            self.chars.next();
+        }
+        Ok(
+            if result == "print" { Token::Print }
+            else if result == "true" { Token::Bool(true) }
+            else if result == "false" { Token::Bool(false) }
+            else if result == "or" { Token::Or }
+            else if result == "and" { Token::And }
+            else if result == "var" { Token::Var }
+            else if result == "if" { Token::If }
+            else if result == "while" { Token::While }
+            else if result == "for" { Token::For }
+            else if result == "fun" { Token::Fun }
+            else if result == "return" { Token::Return }
+            else if result == "class" { Token::Class }
+            else { Token::Ident(result) }
+        )
+    }
+
+    fn read_string(&mut self) -> Result<Token, LexError> {
+        let mut result: String = "".into();
+        loop {
+            match self.chars.peek() {
+                None => { return Err(LexError::UnterminatedStringLiteral); }
+                Some('"') => { self.chars.next(); break; }
+                Some(c) => { result.push(*c); self.chars.next(); }
+            }
+        }
+        Ok(Token::Str(result))
+    }
+
+    fn read_int(&mut self, mut result: i64) -> Result<Token, LexError> {
+        loop {
+            match self.chars.peek() {
+                Some(c) if c.is_digit(NUMBER_BASE) => {
+                    result *= 10;
+                    result += c.to_digit(NUMBER_BASE).unwrap() as i64;
+                }
+                _ => break,
+            }
+            self.chars.next();
+        }
+        Ok(Token::Int(result))
+    }
+}
+
+fn main() {
+    let mut lexer = Lexer::from_str("print \"hello, world!\"; 1 + abc <= 3; 4 < 5; 6 == 7; true; false;
+        var average = (min + max) / 2;");
+    loop {
+        let token = lexer.next();
+        println!("token: {token:?}");
+        if token.is_err() || token == Ok(Token::Eof) { break; }
+    }
+}
